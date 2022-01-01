69cb616f483159fa24c306c2e6b7f0ee22cc382f
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2022-01-01 16:22:08 +0500

    setup folder and file structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,16 @@
+import {
+  Loader,
+  Navbar,
+  Transactions,
+  Footer,
+  Welcome,
+  Services,
+} from './components';
+
 const App = () => {
   return (
-    <div className='App'>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+    <div className='min-h-screen'>
+      <div className='gradient-bg-welcome'></div>
     </div>
   );
 };
