9878a5bf00185b30d13d6a384b9db14c6269045c
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2022-01-01 17:46:24 +0500

    cleaning

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,13 @@ import {
 const App = () => {
   return (
     <div className='min-h-screen'>
-      <div className='gradient-bg-welcome'></div>
+      <div className='gradient-bg-welcome'>
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
     </div>
   );
 };
